edb88631f11be3ff7e9ea2de9d208ddaf613d6ce
11111Asif <asif040502@gmail.com>
2025-05-13 16:35:36 -0700

    added to spnner

--- src/components/Phones/Phones.jsx
+++ src/components/Phones/Phones.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { BarChart, Bar, ResponsiveContainer, XAxis, YAxis, Tooltip } from 'recharts';
-
+// import { Audio } from 'react-loader-spinner'
 
 const Phones = () => {
     const [phones, setPhones] = useState([])
@@ -28,6 +28,15 @@ const Phones = () => {
 
     return (
         <div>
+            {/* <Audio
+                height="80"
+                width="80"
+                radius="9"
+                color="green"
+                ariaLabel="three-dots-loading"
+                wrapperStyle
+                wrapperClass
+            /> */}
             <h3 className="text-5xl">Phones: {phones.length}</h3>
             <BarChart width={1200} height={400} data={phones}>
                 <Bar dataKey="price" fill="#8884d8" />
